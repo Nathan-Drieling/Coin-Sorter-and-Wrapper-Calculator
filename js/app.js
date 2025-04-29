ab69ca3a6e9dc22f64b517d87b829c6bcf95a51b
Nathan Drieling <nathandrieling@gmail.com>
2025-04-29 17:58:26 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -42,19 +42,19 @@ function GenerateRandomSetOfCoins(CoinOptions, length)
     {
         const RandomIndex = Math.floor(Math.random() * CoinOptions.length);
 
-        if (CoinResult.includes("penny "))
+        if (CoinResult.includes(" penny "))
         {
             PennyCounter++;
         }
-        else if (CoinResult.includes("nickel "))
+        else if (CoinResult.includes(" nickel "))
         {
             NickelCounter++;
         }
-        else if (CoinResult.includes("dime "))
+        else if (CoinResult.includes(" dime "))
         {
             DimeCounter++;
         }
-        else if (CoinResult.includes("quarter "))
+        else if (CoinResult.includes(" quarter "))
         {
             QuarterCounter++;
         }
